5b264f0477bde105dfb8b48df41062d5f66f5caa
hungga <vuthehung17@gmail.com>
2016-05-17 01:18:03 +0700

    no message

--- projectCTDL/daoNguocXau.cpp
+++ projectCTDL/daoNguocXau.cpp
@@ -1,47 +1,48 @@
 #include<iostream>
 #include<cstring>
-
 using namespace std;
+typedef char item;
 struct Node {
-	char data;
+	item data;
 	Node *next;
 };
+
 struct Stack {
 	Node *top;
-};
-void initStack(Stack &s) {
-	s.top = NULL;
+} ;
+void initStack(Stack *s) {
+	s->top = NULL;
 }
 Node *initNode(char x) {
 	Node *node = new Node;
 	node->data = x;
 	return node;
 }
-bool isEmpty(Stack &s) {
-	if(s.top == NULL) {
+bool isEmpty(Stack *s) {
+	if(s->top == NULL) {
 		return true;
 	}
 	return false;
 }
-void push(Stack &s, char x) {
+void push(Stack *s, char x) {
 	Node *node = initNode(x);
-		node->next = s.top;
-		s.top = node;
+		node->next = s->top;
+		s->top = node;
 }
-void pop(Stack &s) {
+void pop(Stack *s) {
 	if(isEmpty(s) == true) {
 		cout<<"Ngan xep rong ! \n";
 	} else {
-		Node *p = s.top;
-		s.top = s.top->next;
+		Node *p = s->top;
+		s->top = s->top->next;
 		cout<<p->data;
 	}
 }
-void display(Stack &s) {
+void display(Stack *s) {
 	if(isEmpty(s) == true) {
 		cout<<"Ngan xep rong ! \n";
 	} else {
-		Node *i = s.top;
+		Node *i = s->top;
 		while(i != NULL) {
 			cout<<i->data;
 			i = i->next;
@@ -50,13 +51,12 @@ void display(Stack &s) {
 }
 int main() {
 	Stack s1;
-	initStack(s1);
+	initStack(&s1);
 	string c;
 	getline(cin, c);
 	fflush(stdin);
-
 	for(int i=0; i< c.length(); i++) {
-		push(s1, c[i]);
+		push(&s1, c[i]);
 	}
-	display(s1);
+	display(&s1);
 }
