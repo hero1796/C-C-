62231ec278cb4265d30b6f7fe837589538a8b1a2
hung vu <vuthehung17@gmail.com>
2017-05-11 16:00:20 +0700

    no message

--- codeAcm/notAccepted/P17QPROE.cpp
+++ codeAcm/notAccepted/P17QPROE.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
 using namespace std;
+typedef long long ll;
 int main() {
-	
-	return 0;
+    int t;
+    long a, b, k;
+    vector <long> vtPrime;
+    long n = 1e7 + 20;
+    long* p = NULL;
+    p = new long[n]();
+    p[0] = p[1] = 0;
+    long num;
+    for (num = 2; num < n; ++num) {
+        if (num > 2 && (num % 2) == 0)
+            p[num] = 0;
+        else
+            p[num] = 1;
+    }
+    num = 3;
+    while (num <= sqrt(n)) {
+        for (long j = num; (num * j) <= n; ++j) // chay tu j = num rat hay
+            p[num * j] = 0;
+        do {
+            num += 2;
+        } while (!p[num]);
+    }
+    for (num = 2; num < n; ++num) {
+        if (p[num])
+          vtPrime.push_back(num);
+    }
+    long quotient, temp, cnt;
+    vector<long> vtResult;
+    cin >> t;
+    long *pt = NULL;
+    for (int i = 0; i < t; ++i) {
+        cnt = 0;
+        cin >> a >> b >> k;
+        pt = new long[b - a + 1]();
+        for (vector<long>::iterator it = vtPrime.begin(); *it <= b; ++it) {
+            quotient = b / *it;
+            while((temp = (*it * quotient)) >= a && temp <= b) {
+                pt[temp - a] += 1;
+                quotient--;
+            }
+        }
+        for (long j = 0; j < b - a + 1; ++j)
+            if(pt[j] == k) cnt++;
+        vtResult.push_back(cnt);
+    }
+    int i = 1;
+    for (vector<long>::iterator it = vtResult.begin(); it != vtResult.end(); ++it) {
+        cout <<"Case #" << i << ": " << *it << endl;
+        i++;
+    }
+    return 0;
 }
---
+++ codeAcm/notAccepted/PTIT017A.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+typedef long long ll;
+int main() {
+	long num, n = 1e7 + 1;
+   	long* prime = NULL;
+   	prime = new long[n]();
+   	prime[0] = prime[1] = 0;
+   	for (num = 2; num < n; ++num) {
+       	if (num > 2 && (num % 2) == 0)
+           	prime[num] = 0;
+       	else
+           	prime[num] = 1;
+   	}
+    num = 3;
+    while (num <= sqrt(n)) {
+        for (long j = num; (num * j) <= n; ++j) // chay tu j = num rat hay
+           	prime[num * j] = 0;
+        do {
+        	num += 2;
+        } while (!prime[num]);
+        cout << num << endl;
+    }
+    long a, b, k, quotient, temp, cnt;
+    int t;
+	cin >> t;
+	long res[t];
+	long *pt = NULL;
+	for (int i = 0; i < t; ++i) {
+		cnt = 0;
+		cin >> a >> b >> k;
+		pt = new long[b - a + 1]();
+		for (long j = 2; j <= sqrt(b); ++j) {
+			quotient = b / j;
+			while((temp = (j * quotient)) >= a && temp <= b && j <= sqrt(temp)) {
+				if(prime[j]) pt[temp - a] += 1;
+				if(prime[quotient] && quotient != j) pt[temp - a] += 1;
+				quotient--;
+			}
+		}
+		for (long j = 0; j < b - a + 1; ++j) {
+			if(pt[j] == k && !prime[j + a]) cnt++;
+			if(pt[j] == (k - 1) && prime[j + a]) cnt++;
+		}
+		res[i] = cnt;
+	}
+	for (long i = 0; i < t; i++)
+		cout <<"Case #" << i + 1 << ": " << res[i] << endl;
+    return 0;
+}
